40e675e761777f15226339a6965d821adf153953
Jeffrey-Huang98 <jeffreyhuang98.jh@gmail.com>
2019-10-14 17:51:57 +0800

    part b wip

--- start_project_1a.py
+++ start_project_1a.py
@@ -8,6 +8,17 @@ import pylab as plt
 
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
+NUM_FEATURES = 21
+NUM_CLASSES = 3
+
+learning_rate = 0.01
+epochs = 5000
+batch_size = 32
+num_neurons = 10
+num_folds = 5
+seed = 10
+np.random.seed(seed)
+
 # scale data
 def scale(X, X_min, X_max):
     return (X - X_min)/(X_max-X_min)
@@ -17,52 +28,79 @@ def ffn(x, hidden_units):
     
   with tf.name_scope('hidden'):
     weights = tf.Variable(
-      tf.truncated_normal([no_features, hidden_units],
-                            stddev=1.0 / np.sqrt(float(no_features))),
+      tf.truncated_normal([NUM_FEATURES, hidden_units], stddev=1.0 / np.sqrt(float(NUM_FEATURES))),
         name='weights')
-    biases = tf.Variable(tf.zeros([hidden_units]),
-                         name='biases')
+    biases = tf.Variable(tf.zeros([hidden_units]),name='biases')
     h = tf.nn.relu(tf.matmul(x, weights) + biases)
     
   with tf.name_scope('output'):
     weights = tf.Variable(
-        tf.truncated_normal([hidden_units, 1],
-                            stddev=1.0 / np.sqrt(float(hidden_units))),
+        tf.truncated_normal([hidden_units, NUM_CLASSES], stddev=1.0 / np.sqrt(float(hidden_units))),
         name='weights')
-    biases = tf.Variable(tf.zeros([1]),
-                         name='biases')
+    biases = tf.Variable(tf.zeros([NUM_CLASSES]), name='biases')
     u = tf.nn.softmax(tf.matmul(h, weights) + biases)
     
   return u
 
-NUM_FEATURES = 21
-NUM_CLASSES = 3
+# train the network and find errors
+def train_exp():
+  #read train data
 
-learning_rate = 0.01
-epochs = 5000
-batch_size = 32
-num_neurons = 10
-seed = 10
-np.random.seed(seed)
+  train_input = np.genfromtxt('ctg_data_cleaned.csv', delimiter= ',')
+  #divide 2D array to training set
+  trainX, train_Y = train_input[1:, :21], train_input[1:,-1].astype(int)
+  #normalize the X to [0,1]
+  trainX = scale(trainX, np.min(trainX, axis=0), np.max(trainX, axis=0))
+  #create a matrix of dimension train_y row x 3 filled with zeros 
+  trainY = np.zeros((train_Y.shape[0], NUM_CLASSES))
+  #create classification matrix
+  trainY[np.arange(train_Y.shape[0]), train_Y-1] = 1 #one hot matrix
 
-#read train data
+  # experiment with small datasets
+  # divide to training set and test set (70:30)
+  testX = trainX[:638]
+  testY = trainY[:638]
+  trainX = trainX[638:]
+  trainY = trainY[638:]
 
-train_input = np.genfromtxt('ctg_data_cleaned.csv', delimiter= ',')
-#divide 2D array to training set
-trainX, train_Y = train_input[1:, :21], train_input[1:,-1].astype(int)
-#normalize the X to [0,1]
-trainX = scale(trainX, np.min(trainX, axis=0), np.max(trainX, axis=0))
-#create a matrix of dimension train_y row x 3 filled with zeros 
-trainY = np.zeros((train_Y.shape[0], NUM_CLASSES))
-#create classification matrix
-trainY[np.arange(train_Y.shape[0]), train_Y-1] = 1 #one hot matrix
+  err = []
+  for fold in range(num_folds):
+      start, end = fold*20, (fold+1)*20
+      x_test, y_test = trainX[start:end], trainY[start:end]
+      x_train  = np.append(trainX[:start], trainX[end:], axis=0)
+      y_train = np.append(trainY[:start], trainY[end:], axis=0) 
 
-# experiment with small datasets
-# divide to training set and test set (70:30)
-testX = trainX[:638]
-testY = trainY[:638]
-trainX = trainx[638:]
-trainY = trainY[638:]
+      err_ = []
+      for no_hidden in num_neurons:
+          x = tf.placeholder(tf.float32, [None, NUM_FEATURES])
+          y_ = tf.placeholder(tf.float32, [None, 1])
+
+          y = ffn(x, no_hidden)
+
+          # Create the model
+
+          loss = tf.reduce_mean(tf.reduce_sum(tf.square(y_ - y),axis=1))
+      
+          train = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+      
+          # train
+          with tf.Session() as sess:
+              tf.global_variables_initializer().run()
+
+              for i in range(epochs):
+                  train.run(feed_dict={x:x_train, y_: y_train})
+                  
+              err_.append(loss.eval(feed_dict={x:x_test, y_:y_test}))
+      
+      err.append(err_)
+
+  
+  cv_err = np.mean(np.array(err), axis = 0)
+  print('cv errors {}'.format(cv_err))
+
+  return cv_err
+
+
 
 
 # Create the model
@@ -71,9 +109,9 @@ y_ = tf.placeholder(tf.float32, [None, NUM_CLASSES])
 
 # Build the graph for the deep net
 	
-##weights = tf.Variable(tf.truncated_normal([NUM_FEATURES, NUM_CLASSES], stddev=1.0/math.sqrt(float(NUM_FEATURES))), name='weights')
-##biases  = tf.Variable(tf.zeros([NUM_CLASSES]), name='biases')
-##logits  = tf.matmul(x, weights) + biases
+weights = tf.Variable(tf.truncated_normal([NUM_FEATURES, NUM_CLASSES], stddev=1.0/math.sqrt(float(NUM_FEATURES))), name='weights')
+biases  = tf.Variable(tf.zeros([NUM_CLASSES]), name='biases')
+logits  = tf.matmul(x, weights) + biases
 
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_, logits=logits)
 loss = tf.reduce_mean(cross_entropy)
--- start_project_1b.py
+++ start_project_1b.py
@@ -10,58 +10,87 @@ tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 NUM_FEATURES = 7
 
-learning_rate = 0.01
+learning_rate = 0.001
 epochs = 1000
+
 batch_size = 8
-num_neuron = 30
+num_neuron = 10
 seed = 10
 np.random.seed(seed)
 
-#read and divide data into test and train sets 
-admit_data = np.genfromtxt('admission_predict.csv', delimiter= ',')
-X_data, Y_data = admit_data[1:,1:8], admit_data[1:,-1]
-Y_data = Y_data.reshape(Y_data.shape[0], 1)
+# build a feedforward network
+def ffn(x, hidden_units):
+    
+  with tf.name_scope('hidden'):
+    weights = tf.Variable(tf.truncated_normal([NUM_FEATURES, hidden_units], stddev=1.0 / np.sqrt(float(NUM_FEATURES))), name='weights')
+    biases = tf.Variable(tf.zeros([hidden_units]),name='biases')
+    h = tf.nn.relu(tf.matmul(x, weights) + biases)
+    
+  with tf.name_scope('output'):
+    weights = tf.Variable(tf.truncated_normal([hidden_units, 1], stddev=1.0 / np.sqrt(float(hidden_units))), name='weights')
+    biases = tf.Variable(tf.zeros([1]), name='biases')
+    u = tf.matmul(h, weights) + biases
+    
+  return u
 
-idx = np.arange(X_data.shape[0])
-np.random.shuffle(idx)
-X_data, Y_data = X_data[idx], Y_data[idx]
+# train the network and find errors
+def train_exp(batch_size):
+	#read and divide data into test and train sets 
+	admit_data = np.genfromtxt('admission_predict.csv', delimiter= ',')
+	X_data, Y_data = admit_data[1:,1:8], admit_data[1:,-1]
+	Y_data = Y_data.reshape(Y_data.shape[0], 1)
 
-# experiment with small datasets
-trainX = X_data[:100]
-trainY = Y_data[:100]
+	# divide to training set and test set (70:30)
+	testX = X_data[:120]
+	testY = Y_data[:120]
+	trainX = X_data[120:]
+	trainY = Y_data[120:]
 
-trainX = (trainX- np.mean(trainX, axis=0))/ np.std(trainX, axis=0)
+	trainX = (trainX- np.mean(trainX, axis=0))/ np.std(trainX, axis=0)
 
-# Create the model
-x = tf.placeholder(tf.float32, [None, NUM_FEATURES])
-y_ = tf.placeholder(tf.float32, [None, 1])
+	# Create the model
+	x = tf.placeholder(tf.float32, [None, NUM_FEATURES])
+	y_ = tf.placeholder(tf.float32, [None, 1])
+	
+	# Build the graph for the deep net
+	y = ffn(x, num_neuron)
 
-# Build the graph for the deep net
-weights = tf.Variable(tf.truncated_normal([NUM_FEATURES, 1], stddev=1.0 / np.sqrt(NUM_FEATURES), dtype=tf.float32), name='weights')
-biases = tf.Variable(tf.zeros([1]), dtype=tf.float32, name='biases')
-y = tf.matmul(x, weights) + biases
+	# Create the gradient descent optimizer with the given learning rate.
+	optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+	loss = tf.reduce_mean(tf.square(y_ - y))
+	train_op = optimizer.minimize(loss)
 
+	# train
+	N = len(trainX)
+	idx = np.arange(N)
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
 
+		err = []
+		for i in range(epochs):
+			np.random.shuffle(idx)
+			trainX = trainX[idx]
+			trainY = trainY[idx]
 
-#Create the gradient descent optimizer with the given learning rate.
-optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-loss = tf.reduce_mean(tf.square(y_ - y))
-train_op = optimizer.minimize(loss)
+			for start, end in zip(range(0, N, batch_size), range(batch_size, N, batch_size)):
+				train_op.run(feed_dict={x: trainX[start:end], y_: trainY[start:end]})
 
-with tf.Session() as sess:
-	sess.run(tf.global_variables_initializer())
-	train_err = []
-	for i in range(epochs):
-		train_op.run(feed_dict={x: trainX, y_: trainY})
-		err = loss.eval(feed_dict={x: trainX, y_: trainY})
-		train_err.append(err)
+			test_err = loss.eval(feed_dict={x:testX, y_:testY})
+			if i%10 == 0:
+				print('batch %d: iter %d, test error %g'%(batch_size, i, test_err))
+			err.append(test_err)
 
-		if i % 100 == 0:
-			print('iter %d: train error %g'%(i, train_err[i]))
+	return err
 
-# plot learning curves
-plt.figure(1)
-plt.plot(range(epochs), train_err)
-plt.xlabel(str(epochs) + ' iterations')
-plt.ylabel('Train Error')
-plt.show()
+def main():
+	train_err = train_exp(8)
+
+	# plot learning curves
+	plt.figure(1)
+	plt.plot(range(epochs), train_err)
+	plt.xlabel(str(epochs) + ' iterations')
+	plt.ylabel('Train Error')
+	plt.show()
+
+if __name__ == '__main__':
+	main()
