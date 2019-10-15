885d8e279211808455db5a33c63d0e2ff8e94a56
Jeffrey-Huang98 <jeffreyhuang98.jh@gmail.com>
2019-10-15 20:49:16 +0800

    part a wip

--- start_project_1a.py
+++ start_project_1a.py
@@ -27,22 +27,21 @@ def scale(X, X_min, X_max):
 def ffn(x, hidden_units):
 
   # hidden layer  
-  with tf.name_scope('hidden'):
-    weights = tf.Variable(
-      tf.truncated_normal([NUM_FEATURES, hidden_units], stddev=1.0 / np.sqrt(float(NUM_FEATURES))),
-        name='weights')
-    biases = tf.Variable(tf.zeros([hidden_units]),name='biases')
-    h = tf.nn.relu(tf.matmul(x, weights) + biases)
+  w1 = tf.Variable(
+    tf.truncated_normal([NUM_FEATURES, hidden_units], stddev=1.0 / np.sqrt(float(NUM_FEATURES))),
+      name='weights')
+  b1 = tf.Variable(tf.zeros([hidden_units]),name='biases')
+  h = tf.nn.relu(tf.matmul(x, w1) + b1)
     
   # output layer
-  with tf.name_scope('output'):
-    weights = tf.Variable(
-        tf.truncated_normal([hidden_units, NUM_CLASSES], stddev=1.0 / np.sqrt(float(hidden_units))),
-        name='weights')
-    biases = tf.Variable(tf.zeros([NUM_CLASSES]), name='biases')
-    u = tf.nn.softmax(tf.matmul(h, weights) + biases)
+  w2 = tf.Variable(
+      tf.truncated_normal([hidden_units, NUM_CLASSES], stddev=1.0 / np.sqrt(float(hidden_units))),
+      name='weights')
+  b2 = tf.Variable(tf.zeros([NUM_CLASSES]), name='biases')
+  u = tf.matmul(h, w2) + b2
+  y = tf.nn.softmax(u)
     
-  return u
+  return u, y, w1, w2
 
 # train the network and find errors
 def train_exp(batch_size):
@@ -71,8 +70,28 @@ def train_exp(batch_size):
   # calculate the number of surplus records
   surplus_size = len(trainingX) % num_folds
 
+  # Create the model
+  x = tf.placeholder(tf.float32, [None, NUM_FEATURES])
+  y_ = tf.placeholder(tf.float32, [None, NUM_CLASSES])
+
+  # Build the graph for the deep net
+  u, y, w1, w2 = ffn(x, num_neurons)
+
+  cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_, logits=u)
+  loss = tf.reduce_mean(cross_entropy)
+
+  # loss = tf.reduce_mean(tf.reduce_sum(tf.square(y_ - y),axis=1))
+
+  train = tf.train.GradientDescentOptimizer(learning_rate)
+  train_op = train.minimize(loss)
+
+  correct_prediction = tf.cast(tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1)), tf.float32)
+  accuracy = tf.reduce_mean(correct_prediction)
+
   start_ = 0
-  err = []
+  acc = []
+
+
   for fold in range(num_folds):
     if(surplus_size != 0):
       start, end = start_, (fold+1)*(fold_size + 1)
@@ -87,23 +106,7 @@ def train_exp(batch_size):
     y_train = np.append(trainingY[:start], trainingY[end:], axis=0) 
     print(y_train)
 
-    # Create the model
-    x = tf.placeholder(tf.float32, [None, NUM_FEATURES])
-    y_ = tf.placeholder(tf.float32, [None, NUM_CLASSES])
-
-	  # Build the graph for the deep net
-    y = ffn(x, batch_size)
-
-    cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_, logits=y)
-    loss = tf.reduce_mean(cross_entropy)
-
-    # loss = tf.reduce_mean(tf.reduce_sum(tf.square(y_ - y),axis=1))
-
-    train = tf.train.GradientDescentOptimizer(learning_rate)
-    train_op = train.minimize(loss)
-
-    correct_prediction = tf.cast(tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1)), tf.float32)
-    accuracy = tf.reduce_mean(correct_prediction)
+    
     
     err_ = []
     # train
@@ -120,33 +123,35 @@ def train_exp(batch_size):
         for start, end in zip(range(0, N, batch_size), range(batch_size, N, batch_size)):
           train_op.run(feed_dict={x: x_train[start:end], y_: y_train[start:end]})
           
-        train_acc.append(loss.eval(feed_dict={x:x_test, y_:y_test}))
+        train_acc.append(accuracy.eval(feed_dict={x:x_test, y_:y_test}))
         if i % 100 == 0:
           print('batch %d: iter %d,  accuracy %g'%(batch_size, i, train_acc[i]))
-        err.append(train_acc)
+        # err.append(train_acc)
       
-      err.append(loss.eval(feed_dict={x:x_test, y_:y_test}))
+      acc.append(train_acc)
        
-  cv_err = np.mean(np.array(err), axis = 0)
-  print('cv errors {}'.format(cv_err))
+  # cv_err = np.mean(np.array(err), axis = 0)
+  # print('cv errors {}'.format(cv_err))
 
-  return cv_err, train_acc
+  return acc
 
 
 def main():
 
   # perform experiments
-  err, train_acc = train_exp(32)
+  train_acc = train_exp(32)
 
   # compute mean error
-  mean_err = np.mean(np.array(err), axis = 0)
-  print(mean_err)
+  # mean_err = np.mean(np.array(err), axis = 0)
+  # print(mean_err)
   
   # plot learning curves
   plt.figure(1)
-  plt.plot(range(epochs), train_acc)
+  for i in range(num_folds):
+    plt.plot(range(epochs), train_acc[i], label='folds %g'% i)
   plt.xlabel(str(epochs) + ' iterations')
   plt.ylabel('Train accuracy')
+  plt.legend()
   plt.show()
 
 
